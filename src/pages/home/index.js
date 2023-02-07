88d6e05d29b8218f8969311dabc3970d02a8cf3d
rodneyrinaldi <rodneyrinaldi@hotmail.com>
2023-02-07 00:36:10 -0300

    first commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,36 @@
+import Image from "next/image";
+import Logo from "../../../public/favicon.png";
+import styles from "./index.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <header>
+        <div className={styles.header}>
+          <h1>
+            <Image src={Logo} alt="" height="52" width="52" />
+          </h1>
+          <h1>Pergunta e Resposta</h1>
+        </div>
+      </header>
+
+      <main>
+        <div className={styles.blocks}></div>
+      </main>
+
+      <footer>
+        <div className={styles.footer}>
+          <div>
+            <a>qna.rrs.net.br</a>
+          </div>
+          <div>
+            <a>dev.rodneyrinaldi.com</a>
+          </div>
+          <div>
+            <a>adv.rodneyrinaldi.com</a>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
---
+++ src/pages/moreinfo/index.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "../../../public/favicon2.png";
+import styles from "./index.module.css";
+
+export default function MoreInfo() {
+  return (
+    <main className={styles.container}>
+      <div className={styles.row1}>
+        <div>
+          <Image src={Logo} alt="" height="96" width="96" />
+        </div>
+        <div>
+          <h3>autorizacao</h3>
+          <h3>online</h3>
+        </div>
+      </div>
+
+      <div className={styles.row2}>
+        <h4>Advogado</h4>
+        <p>
+          Conquiste sua segurança necessária para trabalhar com tranquilidade.
+        </p>
+        <p>
+          Disponibilize e gerencie com agilidade os contratos, procurações e
+          controle seus pagamentos.
+        </p>
+        <p>Eficiência e qualidade a por um baixo investimento.</p>
+      </div>
+
+      <div className={styles.row3}>
+        <Link href="/">
+          <button>voltar</button>
+        </Link>
+      </div>
+    </main>
+  );
+}
